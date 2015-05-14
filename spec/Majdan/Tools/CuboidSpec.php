7c90bfb9cd192564678b4ab990f5b53f701b7a7e
majdan <majdanski.dawid@gmail.com>
2015-05-14 11:20:45 +0200

    Majdan/Cuboid: metoda volume

--- spec/Majdan/Tools/CuboidSpec.php
+++ spec/Majdan/Tools/CuboidSpec.php
@@ -11,4 +11,16 @@ class CuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Majdan\Tools\Cuboid');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(456)->getH()->shouldReturn(456);
+    }
+    
+    function it_should_calculate_volume()
+    {
+        $this->setA(2)->setB(2)->setH(2)->volume()->shouldReturn(8);
+    }
 }
