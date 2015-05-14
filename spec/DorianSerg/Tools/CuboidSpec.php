9ccd22ab4b1e5c60a30ce6486147894580adff62
DorianSerg <dorian.sergiel@gmail.com>
2015-05-14 12:19:50 +0200

    Cuboid:implementacja metod

--- spec/DorianSerg/Tools/CuboidSpec.php
+++ spec/DorianSerg/Tools/CuboidSpec.php
@@ -11,4 +11,15 @@ class CuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('DorianSerg\Tools\Cuboid');
     }
-}
+
+     function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+        $this->setB(2)->getB()->shouldReturn(2);
+        $this->setC(4)->getC()->shouldReturn(4);
+    }
+    function it_should_calculate_objetosc()
+	{
+	    $this->setA(2)->setB(2)->setC(4)->objetosc()->shouldReturn(16);
+	}
+    }
