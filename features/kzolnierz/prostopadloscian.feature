c473d1961ab2c41783c73496f8c3817941431910
Krzysztof Zolnierz <zolnierz.93@gmail.com>
2015-05-14 12:14:42 +0200

    kzolnierz/ObjProstopadloscianu: KONIEC

--- features/kzolnierz/prostopadloscian.feature
+++ features/kzolnierz/prostopadloscian.feature
@@ -3,8 +3,8 @@ Feature: Obliczenia
   Scenario: Objetosc prostopadloscianu
     Given I am on homepage
     When I follow "Objetosc prostopadloscianu by kzolnierz"
-    And I fill in "a" with "5"
-    And I fill in "b" with "4"
-    And I fill in "c" with "3"
+    And I fill in "A" with "5"
+    And I fill in "B" with "4"
+    And I fill in "C" with "3"
     And I press "Oblicz"
-    Then I should see "Wynik wynosi 60"
+    Then I should see "Wynik wynosi: 60"
--- spec/kzolnierz/Tools/ObjprostopadloscianuSpec.php
+++ spec/kzolnierz/Tools/ObjprostopadloscianuSpec.php
@@ -11,4 +11,16 @@ class ObjprostopadloscianuSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kzolnierz\Tools\Objprostopadloscianu');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(5)->getA()->shouldReturn(5);
+        $this->setB(4)->getB()->shouldReturn(4);
+        $this->setC(3)->getC()->shouldReturn(3);
+    }
+    
+    function it_should_calculate_obj()
+    {
+    $this->setA(5)->setB(4)->setC(3)->obj()->shouldReturn(60);
+    }
 }
