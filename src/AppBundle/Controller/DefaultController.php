004add92be07aebac20cdf88acc50b6fd975c1a6
Damian Pyc <pycu0007@gmail.com>
2015-05-14 11:59:26 +0200

    dpyc/cuboid: poprawa kotrolera

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,63 +1,14 @@
 <?php
 namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Form\dpycCuboidType;
-use dpyc\Tools\Cuboid;
-class dpycCuboidController extends Controller
+class DefaultController extends Controller
 {
     /**
-     * @Route("/dpyc/cuboid/show/form", name="dpyc_cuboid_show_form")
+     * @Route("/", name="homepage")
      */
-    public function showFormAction()
+    public function indexAction()
     {
-        $cuboid = new Cuboid();
-        $form = $this->createCreateForm($cuboid);
-        return $this->render(
-            'AppBundle:dpycCuboid:form.html.twig',
-            array(
-                'form' => $form->createView()
-            )
-        );
-    }
-    /**
-     * @Route("/dpyc/cuboid/calc", name="dpyc_cuboid_wynik")
-     * @Method("POST")
-     */
-    public function calculateAction(Request $request)
-    {
-        $cuboid = new Cuboid();
-        $form = $this->createCreateForm($cuboid);
-        $form->handleRequest($request);
-        if ($form->isValid()) {
-            return $this->render(
-                'AppBundle:dpycCuboid:wynik.html.twig',
-                array('wynik' => $cuboid->VCuboid())
-            );
-        }
-        return $this->render(
-            'AppBundle:dpycCuboid:form.html.twig',
-            array(
-                'form' => $form->createView()
-            )
-        );
-    }
-    /**
-     * Creates a form...
-     *
-     * @param Cuboid $cuboid The object
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Cuboid $cuboid)
-    {
-        $form = $this->createForm(new dpycCuboidType(), $cuboid, array(
-            'action' => $this->generateUrl('dpyc_cuboid_wynik'),
-            'method' => 'POST',
-        ));
-        $form->add('submit', 'submit', array('label' => 'Oblicz'));
-        return $form;
+        return $this->render('default/index.html.twig');
     }
 }
