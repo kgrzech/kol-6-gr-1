741077b19fe48be4326d939bbd05edd8e8340e96
NosekAdrian <adrian.nosek@windowslive.com>
2015-05-14 11:34:25 +0200

    NosekAdrian->cuboid: implementacja metod

--- spec/NosekAdrian/Tools/CuboidSpec.php
+++ spec/NosekAdrian/Tools/CuboidSpec.php
@@ -11,4 +11,16 @@ class CuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('NosekAdrian\Tools\Cuboid');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(234)->getB()->shouldReturn(234);
+        $this->setC(345)->getC()->shouldReturn(345);
+    }
+    
+    function it_should_calculate_value()
+    {
+        $this->setA(2)->setB(3)->setC(4)->objetoscCuboid()->shouldReturn(24);
+    }
 }
