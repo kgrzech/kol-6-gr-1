24743ad350acbf65b5cbc37414d92a53eb71b3b4
palina888 <palina888@o2.pl>
2015-05-14 12:09:08 +0200

    palina88Prostopadloscian: poprawa

--- src/AppBundle/Controller/palina888ProstopadloscianController.php
+++ src/AppBundle/Controller/palina888ProstopadloscianController.php
@@ -5,16 +5,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\palina888ProstopadloscianType;
-use kustra88\Tools\palina888ProstopadloscianType;
-class kustra88ObjetoscprostopadloscianuController extends Controller
+use palina888\Tools\palina888ProstopadloscianType;
+class palina888ProstopadloscianController extends Controller
 {
     /**
-     * @Route("/palina888/objetoscp/show/form", name="palina888_objetoscp_show_form")
+     * @Route("/palina888/prostopadloscian/show/form", name="palina888_prostopadloscian_show_form")
      */
     public function showFormAction()
     {
-        $objetoscprostopadloscianu = new Objetoscprostopadloscianu();
-        $form = $this->createCreateForm($objetoscprostopadloscianu);
+        $prostopadloscian = new Prostopadloscian();
+        $form = $this->createCreateForm($prostopadloscian);
         return $this->render(
             'AppBundle:palina888ProstopadloscianType:form.html.twig',
             array(
@@ -23,18 +23,18 @@ class kustra88ObjetoscprostopadloscianuController extends Controller
         );
     }
     /**
-     * @Route("/palina888/objetoscp/calc", name="palina888_objetoscp_wynik")
+     * @Route("/palina888/prostopadloscian/calc", name="palina888_prostopadloscian_wynik")
      * @Method("POST")
      */
     public function calculateAction(Request $request)
     {
-        $objetoscprostopadloscianu = new Objetoscprostopadloscianu();
-        $form = $this->createCreateForm($objetoscprostopadloscianu);
+        $prostopadloscian = new Prostopadloscian();
+        $form = $this->createCreateForm($prostopadloscian);
         $form->handleRequest($request);
         if ($form->isValid()) {
             return $this->render(
                 'AppBundle:palina888ProstopadloscianType:wynik.html.twig',
-                    array('wynik' => $objetoscprostopadloscianu->objetosc())
+                    array('wynik' => $prostopadloscian->objetosc())
             );
         }
         return $this->render(
@@ -47,17 +47,17 @@ class kustra88ObjetoscprostopadloscianuController extends Controller
     /**
      * Creates a form...
      *
-     * @param Objetoscprostopadloscianu $objetoscprostopadloscianu The object
+     * @param Prostopadloscian $prostopadloscian The object
      *
      * @return \Symfony\Component\Form\Form The form
      */
-   private function createCreateForm(Objetoscprostopadloscianu $objetoscprostopadloscianu)
+   private function createCreateForm(Prostopadloscian $prostopadloscian)
    {
-       $form = $this->createForm(new palina888ProstopadloscianType(), $objetoscprostopadloscianu, array(
-           'action' => $this->generateUrl('palina888_objetoscp_wynik'),
+       $form = $this->createForm(new palina888ProstopadloscianType(), $prostopadloscian, array(
+           'action' => $this->generateUrl('palina888_prostopadloscian_wynik'),
             'method' => 'POST',
        ));
         $form->add('submit', 'submit', array('label' => 'Oblicz'));
         return $form;
     }
-} 
+}
