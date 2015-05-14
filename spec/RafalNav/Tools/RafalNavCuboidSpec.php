8b4f8f07304dd6a40a93579b7f5e54fb760f393a
Rafal Nawalany <rafalnav@gmail.com>
2015-05-14 11:38:38 +0200

    koncowy

--- spec/RafalNav/Tools/RafalNavCuboidSpec.php
+++ spec/RafalNav/Tools/RafalNavCuboidSpec.php
@@ -11,4 +11,14 @@ class RafalNavCuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('RafalNav\Tools\RafalNavCuboid');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(325)->getH()->shouldReturn(325);
+    }
+    function it_should_calculate_sum()
+    {
+        $this->setA(5)->setB(2)->setH(2)->field()->shouldReturn(20);
+    }
 }
---
+++ src/AppBundle/Controller/RafalNavCuboidController.php
@@ -0,0 +1,63 @@
+<?php
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\RafalNavCuboidType;
+use RafalNav\Tools\RafalNavCuboid;
+class RafalNavCuboidController extends Controller
+{
+    /**
+     * @Route("/rafalnav/cuboid/show/form", name="rafalnav_cuboid_show_form")
+     */
+    public function showFormAction()
+    {
+        $cuboid = new RafalNavCuboid();
+        $form = $this->createCreateForm($cuboid);
+        return $this->render(
+            'AppBundle:RafalNavCuboid:form.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+    /**
+     * @Route("/rafalnav/cuboid/calc", name="rafalnav_cuboid_wynik")
+     * @Method("POST")
+     */
+    public function calculateAction(Request $request)
+    {
+        $cuboid = new RafalNavCuboid();
+        $form = $this->createCreateForm($cuboid);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            return $this->render(
+                'AppBundle:RafalNavCuboid:wynik.html.twig',
+                array('wynik' => $cuboid->field())
+            );
+        }
+        return $this->render(
+            'AppBundle:RafalNavCuboid:form.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+    /**
+     * Creates a form...
+     *
+     * @param Cuboid $cuboid The object
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Cuboid $cuboid)
+    {
+        $form = $this->createForm(new RafalNavCuboidType(), $cuboid, array(
+            'action' => $this->generateUrl('rafalnav_cuboid_wynik'),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Oblicz'));
+        return $form;
+    }
+}
