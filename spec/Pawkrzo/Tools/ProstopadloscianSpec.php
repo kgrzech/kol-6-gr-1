bf90b2c5ab4b3cc2aa78ec59b37823cae1d977a2
pawkrzo <pawkrzo@gmail.com>
2015-05-14 11:08:09 +0200

    Pawkrzo/prostopadloscian: wygenerowana klasa - phpspec

---
+++ spec/Pawkrzo/Tools/ProstopadloscianSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\Pawkrzo\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ProstopadloscianSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Pawkrzo\Tools\Prostopadloscian');
+    }
+}
