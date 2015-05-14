732c3a481acee9537e0321d7ef2872fcd900db9c
palina888 <palina888@o2.pl>
2015-05-14 12:13:15 +0200

    palina88Prostopadloscian: poprawa2

--- src/AppBundle/From/palina888ProstopadloscianType.php
+++ src/AppBundle/From/palina888ProstopadloscianType.php
@@ -3,7 +3,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-class kustra88ObjetoscprostopadloscianuType extends AbstractType
+class palina888ProstopadloscianType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
