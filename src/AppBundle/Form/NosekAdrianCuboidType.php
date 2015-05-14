875545018e903132c062c8ca5672fc8cac02d17b
NosekAdrian <adrian.nosek@windowslive.com>
2015-05-14 11:48:49 +0200

    NosekAdrian->cuboid: menu,testy,klasa,metody

---
+++ src/AppBundle/Form/NosekAdrianCuboidType.php
@@ -0,0 +1,37 @@
+<?php
+namespace AppBundle\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+class NosekAdrianCuboidType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('a')
+            ->add('b')
+            ->add('c')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'NosekAdrian\Tools\Cuboid'
+        ));
+    }
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_NosekAdrian_cuboid';
+    }
+}
