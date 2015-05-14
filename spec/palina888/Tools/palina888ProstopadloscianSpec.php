8b500066da7ba885815cb3a9b9145ea3059747c3
palina888 <palina888@o2.pl>
2015-05-14 11:06:58 +0200

    palina888/prostopadloscian: wygenerowana klasa - phpspec

---
+++ spec/palina888/Tools/palina888ProstopadloscianSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\palina888\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class palina888ProstopadloscianSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('palina888\Tools\palina888Prostopadloscian');
+    }
+}
