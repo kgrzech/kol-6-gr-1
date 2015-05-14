56107a14a65691fe3b8f718e05667313cca9c1f6
choopak <durzynski.r@gmail.com>
2015-05-14 11:17:11 +0200

    choopak/kol-6-gr-1 - prostopadloscian

--- spec/choopak/Tools/ProstopadloscianSpec.php
+++ spec/choopak/Tools/ProstopadloscianSpec.php
@@ -11,4 +11,14 @@ class ProstopadloscianSpec extends ObjectBehavior
     {
         $this->shouldHaveType('choopak\Tools\Prostopadloscian');
     }
+     function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+        $this->setB(3)->getB()->shouldReturn(3);
+        $this->setH(4)->getH()->shouldReturn(4);
+    }
+    function it_should_calculate_objetosc()
+	{
+	    $this->setA(2)->setB(3)->setH(4)->objetosc()->shouldReturn(24);
+	}
 }
