29fe849f78469454eb452838e682d077b7d3b3e7
palina888 <palina888@o2.pl>
2015-05-14 11:26:45 +0200

    palina888/prostopadloscian: settery i gettery

--- src/palina888/Tools/palina888Prostopadloscian.php
+++ src/palina888/Tools/palina888Prostopadloscian.php
@@ -4,4 +4,43 @@ namespace palina888\Tools;
 
 class palina888Prostopadloscian
 {
+        private $a;
+	private $b;
+	private $h;
+
+    public function setA($a)
+  {
+        $this->a = $a;
+        return $this;
+    }
+   public function setB($b)
+    {
+        $this->b = $b;
+        return $this;
+    }
+    public function setH($h)
+    {
+        $this->h = $h;
+        return $this;
+  }
+
+   public function getA()
+    {
+        return $this->a;
+   }
+    public function getB()
+   {
+        return $this->b;
+    }
+    public function getH()
+    {
+        return $this->h;
+    }
+
+    public function objetosc()
+    {
+        return $this->a * $this->b * $this->h;
+    }
+
+   
 }
