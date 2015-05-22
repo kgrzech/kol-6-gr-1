b4457cc5c66db95d6770007c0cea8dd86d1ef7b2
gosialeszczuk <gosialeszczuk@gmail.com>
2015-05-22 09:39:02 +0200

    gosialeszczuk: CuboidSpec.php i Cuboid.php

--- spec/gosialeszczuk/Tools/CuboidSpec.php
+++ spec/gosialeszczuk/Tools/CuboidSpec.php
@@ -11,4 +11,15 @@ class CuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('gosialeszczuk\Tools\Cuboid');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(456)->getA()->shouldReturn(456);
+        $this->setB(852)->getB()->shouldReturn(852);
+    }
+    
+    function it_should_calculate_objetosc()
+	{
+	    $this->setA(3)->setB(4)->setC(5)->objetosc()->shouldReturn(60);
+	}
 }
--- src/gosialeszczuk/Tools/Cuboid.php
+++ src/gosialeszczuk/Tools/Cuboid.php
@@ -4,4 +4,49 @@ namespace gosialeszczuk\Tools;
 
 class Cuboid
 {
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+     
+    
+    public function setC($c)
+    {
+         $this->c = $c;
+
+        return $this;
+           
+    }
+    
+    public function getC()
+    {
+        return $this->c;
+    }
+    
+    public function objetosc()
+    {
+        return $this->a * $this->b * $this->c;
+    }
 }
