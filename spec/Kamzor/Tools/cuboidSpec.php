bcabf312284f57fd4dd012b3cff36c624b019e95
Kamzor <kkamil_jurekk@o2.pl>
2015-05-29 11:35:58 +0200

    kamzor:cuboid metody z implementacja

--- spec/Kamzor/Tools/cuboidSpec.php
+++ spec/Kamzor/Tools/cuboidSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace spec\Kamzor\Tools;
+namespace spec\kustra88\Tools;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -9,6 +9,16 @@ class cuboidSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('Kamzor\Tools\cuboid');
+        $this->shouldHaveType('kamzor\Tools\Cuboid');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(1)->getA()->shouldReturn(1);
+        $this->setB(2)->getB()->shouldReturn(2);
+        $this->setC(3)->getC()->shouldReturn(3);
+    }
+    function it_should_calculate_objetosc()
+	{
+	    $this->setA(1)->setB(2)->setC(3)->objetosc()->shouldReturn(6);
+	}
 }
