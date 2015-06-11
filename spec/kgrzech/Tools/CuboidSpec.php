a2a64476752b0429bdef5ded5ad48dee5098d024
kgrzech <ambros1112@hotmail.com>
2015-06-11 20:54:48 +0200

    wygenerowana klasa phpspec

---
+++ spec/kgrzech/Tools/CuboidSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\kgrzech\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class CuboidSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('kgrzech\Tools\Cuboid');
+    }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(3)->getA()->shouldReturn(3);
+        $this->setB(3)->getB()->shouldReturn(3);
+        $this->setC(2)->getC()->shouldReturn(2);
+    }
+    function it_should_calculate_objetosc()
+	{
+	    $this->setA(3)->setB(3)->setC(2)->objetosc()->shouldReturn(18);
+	}
+}
