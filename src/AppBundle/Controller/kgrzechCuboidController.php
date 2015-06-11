a774c4c35b61a6f232e37702673e6181d758d6a1
kgrzech <ambros1112@hotmail.com>
2015-06-11 21:00:03 +0200

    kontroler

---
+++ src/AppBundle/Controller/kgrzechCuboidController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Form\kgrzechCuboidType;
+use kgrzech\Tools\Cuboid;
+
+class kgrzechCuboidController extends Controller
+{
+    /**
+     * @Route("/kgrzech/cuboid/show/form", name="kgrzech_cuboid_show_form")
+     */
+    public function showFormAction()
+    {
+        $cuboid = new Cuboid();
+        $form = $this->createCreateForm($cuboid);
+        return $this->render(
+            'AppBundle:kgrzechCuboid:form.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+    /**
+     * @Route("/kgrzech/cuboid/calc", name="kgrzech_cuboid_wynik")
+     * @Method("POST")
+     */
+    public function calculateAction(Request $request)
+    {
+        $cuboid = new Cuboid();
+        $form = $this->createCreateForm($cuboid);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            return $this->render(
+                'AppBundle:kgrzechCuboid:wynik.html.twig',
+                array('wynik' => $cuboid->objetosc())
+            );
+        }
+        return $this->render(
+            'AppBundle:kgrzechCuboid:form.html.twig',
+            array(
+                'form' => $form->createView()
+            )
+        );
+    }
+    /**
+     * Creates a form...
+     *
+     * @param Cuboid $cuboid The object
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Cuboid $cuboid)
+    {
+        $form = $this->createForm(new kgrzechCuboidType(), $cuboid, array(
+            'action' => $this->generateUrl('kgrzech_cuboid_wynik'),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Oblicz'));
+        return $form;
+    }
+}
