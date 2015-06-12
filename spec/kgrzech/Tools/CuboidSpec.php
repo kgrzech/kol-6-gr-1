97c5d8b657036ceca5e9b4c883dab446bd0d2c2e
kgrzech <ambros1112@hotmail.com>
2015-06-12 09:37:06 +0200

    implementacja metod

--- spec/kgrzech/Tools/CuboidSpec.php
+++ spec/kgrzech/Tools/CuboidSpec.php
@@ -11,4 +11,14 @@ class CuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kgrzech\Tools\Cuboid');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(3)->getA()->shouldReturn(3);
+        $this->setB(3)->getB()->shouldReturn(3);
+        $this->setC(2)->getC()->shouldReturn(2);
+    }
+    function it_should_calculate_objetosc()
+	{
+	    $this->setA(3)->setB(3)->setC(2)->objetosc()->shouldReturn(18);
+	}
 }
